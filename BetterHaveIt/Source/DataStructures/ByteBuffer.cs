4b90d126721d1e8ef90ad39283041b18435b4961
Matteo Beltrame <bltmtt@gmail.com>
2022-02-20 11:40:22 +0100

    v1.0.8

--- BetterHaveIt/Source/DataStructures/ByteBuffer.cs
+++ BetterHaveIt/Source/DataStructures/ByteBuffer.cs
@@ -4,9 +4,11 @@ using System.Text;
 
 namespace BetterHaveIt.DataStructures;
 
-internal class ByteBuffer
+public class ByteBuffer : IDisposable
 {
-    private List<byte> data;
+    private readonly List<byte> data;
+
+    public int CurrentBufferLength => data.Count;
 
     private ByteBuffer()
     {
@@ -15,27 +17,22 @@ internal class ByteBuffer
 
     public static ByteBuffer Empty() => new ByteBuffer();
 
-    public static ByteBuffer From(IEnumerable<byte> bytes)
+    public static ByteBuffer From(IReadOnlyCollection<byte> bytes)
     {
         ByteBuffer buffer = new ByteBuffer();
-        foreach (var b in bytes)
-        {
-            buffer.data.Add(b);
-        }
+        buffer.data.AddRange(bytes);
         return buffer;
     }
+
+    public static implicit operator ReadOnlySpan<byte>(ByteBuffer buffer) => buffer.data.ToArray();
 
     public static implicit operator byte[](ByteBuffer buffer) => buffer.data.ToArray();
 
     public byte[] ToBytes() => data.ToArray();
 
-    public ByteBuffer Write(ulong val)
-    {
-        data.AddRange(BitConverter.GetBytes(val));
-        return this;
-    }
+    #region Write
 
-    public ByteBuffer Write(bool val)
+    public ByteBuffer Write(ushort val)
     {
         data.AddRange(BitConverter.GetBytes(val));
         return this;
@@ -47,9 +44,51 @@ internal class ByteBuffer
         return this;
     }
 
+    public ByteBuffer Write(ulong val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(short val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(int val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(long val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(float val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(double val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
+    public ByteBuffer Write(bool val)
+    {
+        data.AddRange(BitConverter.GetBytes(val));
+        return this;
+    }
+
     public ByteBuffer Write(string val)
     {
-        Write((uint)val.Length);
+        Write(val.Length);
         data.AddRange(Encoding.UTF8.GetBytes(val));
         return this;
     }
@@ -60,39 +99,89 @@ internal class ByteBuffer
         return this;
     }
 
+    #endregion Write
+
+    #region Read
+
+    public ushort ReadUShort()
+    {
+        var buf = data.Take(sizeof(ushort)).ToArray();
+        data.RemoveRange(0, sizeof(ushort));
+        return BitConverter.ToUInt16(buf);
+    }
+
     public uint ReadUInt()
     {
         var buf = data.Take(sizeof(uint)).ToArray();
-        data = data.Skip(sizeof(uint)).ToList();
+        data.RemoveRange(0, sizeof(uint));
         return BitConverter.ToUInt32(buf);
     }
 
     public ulong ReadULong()
     {
         var buf = data.Take(sizeof(ulong)).ToArray();
-        data = data.Skip(sizeof(ulong)).ToList();
+        data.RemoveRange(0, sizeof(ulong));
         return BitConverter.ToUInt64(buf);
+    }
+
+    public short ReadShort()
+    {
+        var buf = data.Take(sizeof(short)).ToArray();
+        data.RemoveRange(0, sizeof(short));
+        return BitConverter.ToInt16(buf);
+    }
+
+    public int ReadInt()
+    {
+        var buf = data.Take(sizeof(int)).ToArray();
+        data.RemoveRange(0, sizeof(int));
+        return BitConverter.ToInt32(buf);
+    }
+
+    public long ReadLong()
+    {
+        var buf = data.Take(sizeof(long)).ToArray();
+        data.RemoveRange(0, sizeof(long));
+        return BitConverter.ToInt64(buf);
+    }
+
+    public float ReadFloat()
+    {
+        var buf = data.Take(sizeof(float)).ToArray();
+        data.RemoveRange(0, sizeof(float));
+        return BitConverter.ToSingle(buf);
+    }
+
+    public double ReadDouble()
+    {
+        var buf = data.Take(sizeof(double)).ToArray();
+        data.RemoveRange(0, sizeof(double));
+        return BitConverter.ToDouble(buf);
+    }
+
+    public bool ReadBool()
+    {
+        var buf = data.Take(sizeof(bool)).ToArray();
+        data.RemoveRange(0, sizeof(bool));
+        return BitConverter.ToBoolean(buf);
     }
 
     public string ReadString()
     {
-        var length = ReadUInt();
-        var buf = data.Take((int)length).ToArray();
-        data = data.Skip((int)length).ToList();
+        var length = ReadInt();
+        var buf = data.Take(length).ToArray();
+        data.RemoveRange(0, length);
         return Encoding.UTF8.GetString(buf);
     }
 
     public char ReadChar()
     {
         var buf = data.Take(sizeof(char)).ToArray();
-        data = data.Skip(sizeof(char)).ToList();
+        data.RemoveRange(0, sizeof(char));
         return BitConverter.ToChar(buf);
     }
 
-    public bool ReadBool()
-    {
-        var buf = data.Take(sizeof(bool)).ToArray();
-        data = data.Skip(sizeof(bool)).ToList();
-        return BitConverter.ToBoolean(buf);
-    }
+    #endregion Read
+
+    public void Dispose() => data.Clear();
 }
